35d1e8bd1bf772328c90c13a28062d336b261362
simonoppowa <simon.oppowa@st.oth-regensburg.de>
2018-03-19 18:22:50 +0100

    added releaseDate

--- app/src/main/java/com/github/simonoppowa/popularmoviesappstage1/model/Movie.java
+++ app/src/main/java/com/github/simonoppowa/popularmoviesappstage1/model/Movie.java
@@ -1,6 +1,7 @@
 package com.github.simonoppowa.popularmoviesappstage1.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by Simon on 14.03.2018.
@@ -14,15 +15,17 @@ public class Movie implements Serializable{
     private String overview;
     private String imagePath;
     private float userRating;
+    private Date releaseDate;
 
 
-    public Movie(int id, String title, String originalTitle, String overview, String poster_path, float userRating) {
+    public Movie(int id, String title, String originalTitle, String overview, String poster_path, float userRating, Date releaseDate) {
         this.id = id;
         this.title = title;
         this.originalTitle = originalTitle;
         this.overview = overview;
         this.imagePath = poster_path;
         this.userRating = userRating;
+        this.releaseDate = releaseDate;
     }
 
     public int getId() {
@@ -75,5 +78,13 @@ public class Movie implements Serializable{
 
     public void setUserRating(float userRating) {
         this.userRating = userRating;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
     }
 }
--- app/src/main/java/com/github/simonoppowa/popularmoviesappstage1/utilities/JSONUtils.java
+++ app/src/main/java/com/github/simonoppowa/popularmoviesappstage1/utilities/JSONUtils.java
@@ -8,7 +8,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,13 +28,15 @@ public class JSONUtils {
     private static final String JSON_OVERVIEW_TAG = "overview";
     private static final String JSON_IMAGE_PATH_TAG = "poster_path";
     private static final String JSON_USER_RATING_TAG = "vote_average";
+    private static final String JSON_USER_RELEASEDATE_TAG = "release_date";
 
     public static List<Movie> getMovieListFromJSON(String jsonString) throws JSONException {
 
         //Movie attributes
         int id;
-        String title, originalTitle, overview, imagePath;
+        String title, originalTitle, overview, imagePath, releaseDateString;
         float userRating;
+        Date releaseDate;
 
         //MovieList
         List<Movie> movieList = new ArrayList<>();
@@ -61,14 +66,31 @@ public class JSONUtils {
             //movie imagePath
             imagePath = resultsJsonArray.getJSONObject(i).getString(JSON_IMAGE_PATH_TAG);
 
-            //movie user
+            //movie userRating
             userRating = (float) resultsJsonArray.getJSONObject(i).getDouble(JSON_USER_RATING_TAG);
 
+            //movie releaseDate
+            releaseDateString = resultsJsonArray.getJSONObject(i).getString(JSON_USER_RELEASEDATE_TAG);
+            releaseDate = convertDate(releaseDateString);
+
             //movie object
-            Movie newMovie = new Movie(id, title, originalTitle, overview, imagePath, userRating);
+            Movie newMovie = new Movie(id, title, originalTitle, overview, imagePath, userRating, releaseDate);
 
             movieList.add(newMovie);
         }
         return movieList;
+    }
+
+    private static Date convertDate(String releaseDateString) {
+        SimpleDateFormat simpleReleaseDate = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date releaseDate = null;
+        try {
+            releaseDate = simpleReleaseDate.parse(releaseDateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return releaseDate;
     }
 }
